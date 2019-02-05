a389209b2eab19a6ac917390484c8f2c57756d52
Cesare Camurani <cesarecamurani@Eleni-PC.home>
2019-02-05 17:20:27 +0000

    test for land changing the status to true passing

--- plane.py
+++ plane.py
@@ -2,3 +2,9 @@ class Plane:
 
     def __init__(self):
         self.flyingStatus = True
+
+    def land(self):
+        self.flyingStatus = False
+
+    def takeoff(self):
+        self.flyingStatus = True
--- test_plane.py
+++ test_plane.py
@@ -11,5 +11,14 @@ class TestPlane(unittest.TestCase):
     def test_flyingStatus_is_true_on_init(self):
         self.assertTrue(self.plane.flyingStatus)
 
+    def test_land_change_flyingStatus_to_false(self):
+        self.plane.land()
+        self.assertFalse(self.plane.flyingStatus)
+
+    def test_takeoff_change_flyingStatus_to_true(self):
+        self.plane.land()
+        self.plane.takeoff()
+        self.assertTrue(self.plane.flyingStatus)
+
 if __name__ == '__main__':
     unittest.main()
