4fbdc33b132c7213b4a6d37dabc18702e5148d07
Cesare Camurani <cesarecamurani@Eleni-PC.home>
2019-02-05 16:47:55 +0000

    test for airport permitting to land passing

--- airport.py
+++ airport.py
@@ -1,2 +1,7 @@
-class Airport():
-    hangar = []
+class Airport:
+
+    def __init__(self):
+        self.hangar = []
+
+    def permit_landing(self, plane):
+        self.hangar.append(plane)
--- test_airport.py
+++ test_airport.py
@@ -1,12 +1,22 @@
 import unittest
+from mock import Mock
 from airport import Airport
 
 class TestAirport(unittest.TestCase):
+
+    def setUp(self):
+        self.airport = Airport()
+        self.plane = Mock()
 
     def test_airport_has_an_hangar(self):
         airport = Airport()
         assert airport.hangar == []
 
+    def test_permit_landing(self):
+        self.airport.permit_landing(self.plane)
+        self.assertIn(self.plane, self.airport.hangar)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
